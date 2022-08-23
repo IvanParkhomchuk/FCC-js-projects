524cd5d938d9ac9fa8221cf41fc283fea89111b1
Ivan <workaccbyivan@gmail.com>
2022-08-23 17:14:29 +0300

    'ceasarsCipher/feature/programFixes'

--- caesarsCipher/script.js
+++ caesarsCipher/script.js
@@ -7,46 +7,18 @@ const copied = document.getElementById('copied');
 const TIMEOUT = 3000;
 
 function rot13(str) {
-    let rot13 = {
-        A: 'N',
-        B: 'O',
-        C: 'P',
-        D: 'Q',
-        E: 'R',
-        F: 'S',
-        G: 'T',
-        H: 'U',
-        I: 'V',
-        J: 'W',
-        K: 'X',
-        L: 'Y',
-        M: 'Z',
-        N: 'A',
-        O: 'B',
-        P: 'C',
-        Q: 'D',
-        R: 'E',
-        S: 'F',
-        T: 'G',
-        U: 'H',
-        V: 'I',
-        W: 'J',
-        X: 'K',
-        Y: 'L',
-        Z: 'M', 
-    };
-  
-    let regex = /\W/;
-    
     let arr = str.toUpperCase().split('');
     let newArr = [];
-  
+
     for (let i = 0; i < arr.length; i++) {
-        if (regex.test(arr[i])) {
+        if (arr[i].charCodeAt() < 65 || arr[i].charCodeAt() > 90) {
             newArr.push(arr[i]);
-            continue;
         }
-        newArr.push(rot13[arr[i]]);
+        else if (arr[i].charCodeAt() + 13 > 90) {
+            newArr.push(String.fromCharCode(arr[i].charCodeAt() - 13));
+        } else {
+            newArr.push(String.fromCharCode(arr[i].charCodeAt() + 13));
+        }
     }
   
     return newArr.join('');
@@ -70,6 +42,6 @@ form.addEventListener('submit', function(e) {
 copyButton.addEventListener('click', function() {
     navigator.clipboard.writeText(result.textContent);
     copied.classList.add('move');
+
     timer();
-    clearTimeout(timer());
 });
