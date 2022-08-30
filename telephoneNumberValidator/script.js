a8270c0f3fea1577b44fa58c7c5c8c2c5a24ba37
Ivan <workaccbyivan@gmail.com>
2022-08-30 16:39:20 +0300

    'telephoneNumberValidator/feature/codeFix'

--- telephoneNumberValidator/script.js
+++ telephoneNumberValidator/script.js
@@ -1,11 +1,11 @@
 const form = document.getElementById('my-form');
 let userInput = document.getElementById('user-input');
-let result = document.getElementById('is-palindrome');
+let result = document.getElementById('is-valid-number');
 
 const TIMEOUT = 3000;
 
 function telephoneCheck(str) {
-    let regex = /^1{0,1}\s*(\(\d{3}\)|\d{3})(\s*|-)\d{3}(\s*|-)\d{4}$/;
+    const regex = /^1{0,1}\s*(\(\d{3}\)|\d{3})(\s*|-)\d{3}(\s*|-)\d{4}$/;
 
     if (regex.test(str)) return true;
     
@@ -15,7 +15,6 @@ function telephoneCheck(str) {
 function timer() {
     return setTimeout(() => {
         result.textContent = '';
-        result.style.color = 'black';
         result.classList.remove('move');
     }, TIMEOUT)
 }
@@ -23,10 +22,12 @@ function timer() {
 function showResult(phoneNumber) {
     if (phoneNumber) {
         result.textContent = `${userInput.value} is valid telephone number!`;
-        result.style.color = 'green';
+        result.classList.remove('red-text');
+        result.classList.add('green-text');
     } else {
         result.textContent = `${userInput.value} is not valid telephone number!`;
-        result.style.color = 'red';
+        result.classList.remove('green-text');
+        result.classList.add('red-text');
     }
 
     result.classList.add('move');
